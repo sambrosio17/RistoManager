49060365d3ad53defb6ae4ca6109e6dca740a158
comarco99 <72262516+comarco99@users.noreply.github.com>
2021-01-26 21:38:28 +0100

    aggiornamento login

--- RistoManager/src/it/RistoManager/Control/Utente/LoginControl.java
+++ RistoManager/src/it/RistoManager/Control/Utente/LoginControl.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import it.RistoManager.DAO.AccountStaffDAO;
 import it.RistoManager.Model.AccountStaffBean;
@@ -25,31 +26,72 @@ public class LoginControl extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String email = request.getParameter("email");
-		String password = request.getParameter("password");
-		
 		String redirectedPage = "";
+		HttpSession session = request.getSession();
+		session.setAttribute("errorType", null);
+		session.setAttribute("error", null);
 		try {
-			String ruolo = checkLogin(email, password);
+		
+			String email = request.getParameter("email");
+			String password = request.getParameter("password");
 			
-			if(ruolo.equalsIgnoreCase("gestione")) {
-				request.getSession().setAttribute("ruolo", "gestione");
-				redirectedPage = "/gestione/index.jsp";
-			} else if(ruolo.equalsIgnoreCase("sala")) {
-				request.getSession().setAttribute("ruolo", "sala");
-				redirectedPage = "/sala/index.jsp";
-			} else if(ruolo.equalsIgnoreCase("cucina")) {
-				request.getSession().setAttribute("ruolo", "cucina");
-				request.setAttribute("fromLogin", true);
-				redirectedPage = "/cucina/index.jsp";
+			
+			if((email.length())==0) {
+				request.setAttribute("errorTest", "Il login non va a buon fine poiché il campo email è vuoto");
+				session.setAttribute("errorType","email");
+				session.setAttribute("error", "il campo non rispetta la lunghezza minima");
+				response.sendRedirect(("./login.jsp"));
+				return;
+			}
+			else if(!(email.matches("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$"))){
+				request.setAttribute("errorTest", "Il login non va a buon fine poiché il formato mail è errato");
+				session.setAttribute("errorType","email");
+				session.setAttribute("error", "il campo non rispetta il formato");
+				response.sendRedirect(("./login.jsp"));
+				return;
+			}
+			else if(password.length()==0){
+				request.setAttribute("errorTest", "Il login non va a buon fine poiché il campo password è vuoto");
+				session.setAttribute("errorType","password");
+				session.setAttribute("error", "il campo non rispetta la lunghezza minima");
+				response.sendRedirect(("./login.jsp"));
+				return;
+			}else {
+		
+				String ruolo = checkLogin(email, password);
+				
+				if(ruolo.equalsIgnoreCase("gestione")) {
+					request.setAttribute("errorTest", "Il login viene effettuato correttamente");
+					
+					request.getSession().setAttribute("ruolo", "gestione");
+					redirectedPage = "/gestione/index.jsp";
+				} else if(ruolo.equalsIgnoreCase("sala")) {
+					request.setAttribute("errorTest", "Il login viene effettuato correttamente");
+					
+					request.getSession().setAttribute("ruolo", "sala");
+					redirectedPage = "/sala/index.jsp";
+				} else if(ruolo.equalsIgnoreCase("cucina")) {
+					request.setAttribute("errorTest", "Il login viene effettuato correttamente");
+					
+					request.getSession().setAttribute("ruolo", "cucina");
+					request.setAttribute("fromLogin", true);
+					redirectedPage = "/cucina/index.jsp";
+				}else {
+					
+					request.setAttribute("errorTest", "Il login non va a buon fine poiché non viene trovata corrispondenza");
+					session.setAttribute("errorType","emailPassword");
+					session.setAttribute("error", "non esiste corrispondenza");
+					redirectedPage= "/login.jsp";	
+				}
+				
 			}
 			
+			response.sendRedirect(request.getContextPath() + redirectedPage);
 		} catch (Exception e) {
 			System.out.println("LOGIN ERROR");
 			request.getSession().setAttribute("loginError", "Credenziali non valide");
-			redirectedPage = "/login.jsp";
 		}
-		response.sendRedirect(request.getContextPath() + redirectedPage);
+
 	}
 	
 	private String checkLogin(String email, String password) throws Exception {
@@ -63,7 +105,7 @@ public class LoginControl extends HttpServlet {
 		System.out.println(account.retrieveAll());
 		
 		if (bean == null || !bean.getPassword().equals(password)) {
-			throw new Exception("Invalid login and password");
+			return "invalid";
 		} else {
 			return bean.getRuolo().toString();
 		}
