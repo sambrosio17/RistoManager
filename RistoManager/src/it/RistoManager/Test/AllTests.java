2b6420131750e6acc34662be847d0a4fc0b95a17
Simone Benitozzi <72170256+simonebenitozzi@users.noreply.github.com>
2021-02-10 23:54:05 +0100

    AllTests

---
+++ RistoManager/src/it/RistoManager/Test/AllTests.java
@@ -0,0 +1,12 @@
+package it.RistoManager.Test;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("it.RistoManager.Test")
+public class AllTests {
+
+}
