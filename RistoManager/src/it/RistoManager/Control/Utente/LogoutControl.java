a842c2e9064bb02ce2f27809dd697b62287b7bca
Simone Benitozzi <72170256+simonebenitozzi@users.noreply.github.com>
2021-02-11 00:07:22 +0100

    Servlet Login Logout

---
+++ RistoManager/src/it/RistoManager/Control/Utente/LogoutControl.java
@@ -0,0 +1,37 @@
+package it.RistoManager.Control.Utente;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 
+ * Permette di eseguire il Logout dalla piattaforma
+ *
+ */
+
+@WebServlet("/logout")
+public class LogoutControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+    public LogoutControl() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getSession().removeAttribute("ruolo");
+		request.getSession().invalidate();
+
+		String redirectedPage = "/login.jsp";
+		response.sendRedirect(request.getContextPath() + redirectedPage);	
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
