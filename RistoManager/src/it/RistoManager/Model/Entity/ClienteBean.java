33deb71ee1c8e878e80fce3d5abd55624e2bba82
Simone Benitozzi <72170256+simonebenitozzi@users.noreply.github.com>
2021-02-11 22:08:52 +0100

    Bean Prodotto e Cliente

--- RistoManager/src/it/RistoManager/Model/Entity/ClienteBean.java
+++ RistoManager/src/it/RistoManager/Model/Entity/ClienteBean.java
@@ -3,46 +3,53 @@ package it.RistoManager.Model.Enity;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import it.RistoManager.FIA.KMeansExecutor;
+
 /**
  * 
  * Rappresenta un cliente del locale
  *
  */
 public class ClienteBean {
-	
+
 	private int id;
 	private String nome, cognome, cellulare, numeroDocumento, email, codiceTavolo;
 	private LocalDate data;
 	private int numeroPosti;
 	private LocalTime ora;
-	
+
+	private int[] preferenze;
+
 	/**
 	 * Costruttore con parametri di default
 	 */
 	public ClienteBean() {
-		id=-1;
-		nome="";
-		cognome="";
-		cellulare="";
-		numeroDocumento="";
-		email="";
-		codiceTavolo="";
-		data=LocalDate.MIN;
-		numeroPosti=-1;
-		ora=LocalTime.MIN;
+		id = -1;
+		nome = "";
+		cognome = "";
+		cellulare = "";
+		numeroDocumento = "";
+		email = "";
+		codiceTavolo = "";
+		data = LocalDate.MIN;
+		numeroPosti = -1;
+		ora = LocalTime.MIN;
+		preferenze = new int[KMeansExecutor.NUM_CLUSTERS];
+		setPreferenze();
 	}
-	
+
 	/**
 	 * Costruttore con parametri
-	 * @param nome Nome del cliente
-	 * @param cognome Cognome del cliente
-	 * @param cellulare Numero di Cellulare del cliente
+	 * 
+	 * @param nome            Nome del cliente
+	 * @param cognome         Cognome del cliente
+	 * @param cellulare       Numero di Cellulare del cliente
 	 * @param numeroDocumento Numero del Documento di identificazione del cliente
-	 * @param email Email del cliente
-	 * @param codiceTavolo Codice Tavolo del cliente
-	 * @param data Data di arrivo del cliente
-	 * @param numeroPosti Numero di posti del tavolo del cliente
-	 * @param ora Orario di arrivo del cliente
+	 * @param email           Email del cliente
+	 * @param codiceTavolo    Codice Tavolo del cliente
+	 * @param data            Data di arrivo del cliente
+	 * @param numeroPosti     Numero di posti del tavolo del cliente
+	 * @param ora             Orario di arrivo del cliente
 	 */
 	public ClienteBean(String nome, String cognome, String cellulare, String numeroDocumento, String email,
 			String codiceTavolo, LocalDate data, int numeroPosti, LocalTime ora) {
@@ -56,8 +63,10 @@ public class ClienteBean {
 		this.data = data;
 		this.numeroPosti = numeroPosti;
 		this.ora = ora;
+		preferenze = new int[KMeansExecutor.NUM_CLUSTERS];
+		setPreferenze();
 	}
-	
+
 	/**
 	 * 
 	 * @return ID del cliente
@@ -65,7 +74,7 @@ public class ClienteBean {
 	public int getId() {
 		return id;
 	}
-	
+
 	/**
 	 * 
 	 * @param id ID del cliente
@@ -73,6 +82,7 @@ public class ClienteBean {
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	/**
 	 * 
 	 * @return Nome del cliente
@@ -80,7 +90,7 @@ public class ClienteBean {
 	public String getNome() {
 		return nome;
 	}
-	
+
 	/**
 	 * 
 	 * @param nome Nome del cliente
@@ -88,7 +98,7 @@ public class ClienteBean {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	
+
 	/**
 	 * 
 	 * @return Cognome del Cliente
@@ -96,7 +106,7 @@ public class ClienteBean {
 	public String getCognome() {
 		return cognome;
 	}
-	
+
 	/**
 	 * 
 	 * @param cognome Cognome del cliente
@@ -104,7 +114,7 @@ public class ClienteBean {
 	public void setCognome(String cognome) {
 		this.cognome = cognome;
 	}
-	
+
 	/**
 	 * 
 	 * @return Numero di cellulare del cliente
@@ -112,7 +122,7 @@ public class ClienteBean {
 	public String getCellulare() {
 		return cellulare;
 	}
-	
+
 	/**
 	 * 
 	 * @param cellulare Numero di cellulare del cliente
@@ -120,7 +130,7 @@ public class ClienteBean {
 	public void setCellulare(String cellulare) {
 		this.cellulare = cellulare;
 	}
-	
+
 	/**
 	 * 
 	 * @return Numero del documento di identificazione del cliente
@@ -128,7 +138,7 @@ public class ClienteBean {
 	public String getNumeroDocumento() {
 		return numeroDocumento;
 	}
-	
+
 	/**
 	 * 
 	 * @param numeroDocumento Numero del documento di identificazione del cliente
@@ -136,7 +146,7 @@ public class ClienteBean {
 	public void setNumeroDocumento(String numeroDocumento) {
 		this.numeroDocumento = numeroDocumento;
 	}
-	
+
 	/**
 	 * 
 	 * @return Email del cliente
@@ -144,7 +154,7 @@ public class ClienteBean {
 	public String getEmail() {
 		return email;
 	}
-	
+
 	/**
 	 * 
 	 * @param email Email del cliente
@@ -152,7 +162,7 @@ public class ClienteBean {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	/**
 	 * 
 	 * @return COdice Tavolo del cliente
@@ -160,7 +170,7 @@ public class ClienteBean {
 	public String getCodiceTavolo() {
 		return codiceTavolo;
 	}
-	
+
 	/**
 	 * 
 	 * @param codiceTavolo Codice Tavolo del cliente
@@ -168,7 +178,7 @@ public class ClienteBean {
 	public void setCodiceTavolo(String codiceTavolo) {
 		this.codiceTavolo = codiceTavolo;
 	}
-	
+
 	/**
 	 * 
 	 * @return Data di arrivo del cliente
@@ -176,7 +186,7 @@ public class ClienteBean {
 	public LocalDate getData() {
 		return data;
 	}
-	
+
 	/**
 	 * 
 	 * @param data Data di arrivo del cliente
@@ -184,7 +194,7 @@ public class ClienteBean {
 	public void setData(LocalDate data) {
 		this.data = data;
 	}
-	
+
 	/**
 	 * 
 	 * @return Numero di posti al Tavolo del cliente
@@ -192,7 +202,7 @@ public class ClienteBean {
 	public int getNumeroPosti() {
 		return numeroPosti;
 	}
-	
+
 	/**
 	 * 
 	 * @param numeroPosti Numero di posti al Tavolo del cliente
@@ -200,7 +210,7 @@ public class ClienteBean {
 	public void setNumeroPosti(int numeroPosti) {
 		this.numeroPosti = numeroPosti;
 	}
-	
+
 	/**
 	 * 
 	 * @return Ora di arrivo del cliente
@@ -208,7 +218,7 @@ public class ClienteBean {
 	public LocalTime getOra() {
 		return ora;
 	}
-	
+
 	/**
 	 * 
 	 * @param ora Ora di arrivo del cliente
@@ -217,13 +227,33 @@ public class ClienteBean {
 		this.ora = ora;
 	}
 
+	public int[] getPreferenze() {
+		return preferenze;
+	}
+	
+	public void setPreferenze() {
+		for (int i = 0; i < preferenze.length; i++)
+			preferenze[i] = 100;
+	}
+
+	public int[] updatePreferenze(int incremento, int cluster) {
+		
+		preferenze[cluster] += incremento;
+		
+		incremento *= -1;
+		incremento /= (preferenze.length - 1);
+		for (int i = 0; i < preferenze.length; i++)
+			if(i != cluster)
+				preferenze[i] += incremento;
+		
+		return preferenze;
+	}
+
 	@Override
 	public String toString() {
 		return "ClienteBean [id=" + id + ", nome=" + nome + ", cognome=" + cognome + ", cellulare=" + cellulare
 				+ ", numeroDocumento=" + numeroDocumento + ", email=" + email + ", codiceTavolo=" + codiceTavolo
 				+ ", data=" + data + ", numeroPosti=" + numeroPosti + ", ora=" + ora + "]";
 	}
-	
-	
 
 }
